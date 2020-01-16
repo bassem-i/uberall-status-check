09a7ea723c501291586184e13481623c3b5995b9
Bassem Mohamed <bassemmohamed1994@gmail.com>
2020-01-16 13:01:27 +0200

    uberallService getListing function

--- src/service/UberallApiService.js
+++ src/service/UberallApiService.js
@@ -4,14 +4,24 @@ const UBERALL_SERVER = "https://sandbox.uberall.com/api";
 const PUBLIC_KEY =
   "GNpPyrkYiTS5BV4F8XckW3kYurprwSk7cRG3Z4jOtrTTBceyfPveAOvDFk3mYY0ofundf";
 
-function searchListings(searchObj, onSuccess, onError) {
+function searchBusiness(searchObj, onSuccess, onError) {
   searchObj["public_key"] = PUBLIC_KEY;
   axios
     .post(`${UBERALL_SERVER}/search`, searchObj)
-    .then(onSuccess)
+    .then(res => onSuccess(res.data.response.searchData))
+    .catch(onError);
+}
+
+function getListing(id, token, directory, onSuccess, onError) {
+  const params = `?token=${token}&directory=${directory}&public_key=${PUBLIC_KEY}`;
+
+  axios
+    .get(`${UBERALL_SERVER}/search/${id}${params}`)
+    .then(res => onSuccess(res.data.response))
     .catch(onError);
 }
 
 export default {
-  searchListings
+  searchBusiness,
+  getListing
 };
