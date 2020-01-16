3a3aa15b4186053f32d84a3e6c0f7c9a96bbf7e9
Bassem Mohamed <bassemmohamed1994@gmail.com>
2020-01-16 16:46:31 +0200

    Directory logo

--- src/UI/ListingCard/ListingCard.js
+++ src/UI/ListingCard/ListingCard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { Descriptions } from "antd";
+import logos from "../../assets/json/directory_logos";
 var moment = require("moment");
 
 const Card = styled.div`
   margin: 1rem 0;
+
+  img {
+    width: 25px;
+    margin-right: 2px;
+  }
 `;
 const Redspan = styled.span`
   color: red;
@@ -29,7 +35,12 @@ const ListingCard = ({
   <Card>
     <Descriptions bordered column={{ xs: 1, md: 2 }} size="small">
       <Descriptions.Item label="Directory Type">
-        {isValue(directoryType)}
+        <>
+          {directoryType && (
+            <img alt={directoryType} src={logos[directoryType]} />
+          )}
+          {isValue(directoryType)}
+        </>
       </Descriptions.Item>
       <Descriptions.Item label="Name">{isValue(name)}</Descriptions.Item>
       <Descriptions.Item label="Street">{isValue(street)}</Descriptions.Item>
